9c6db0861a2eb34961339927ced5d0bfd3856889
jixiangup <jixiangup@gmail.com>
2024-05-14 12:11:29 +0800

    feat: added types json supported

--- api/types.go
+++ api/types.go
@@ -10,22 +10,22 @@ const (
 
 // ConnectionInfo 连接信息
 type ConnectionInfo struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	Database string
+	Host     string `json:"host,omitempty"`
+	Port     int    `json:"port,omitempty"`
+	User     string `json:"user,omitempty"`
+	Password string `json:"password,omitempty"`
+	Database string `json:"database,omitempty"`
 }
 
 // TableSchema defines the schema of a database table
 type TableSchema struct {
-	Columns []ColumnSchema
+	Columns []ColumnSchema `json:"columns,omitempty"`
 }
 
 // ColumnSchema defines the schema of a column in a database table
 type ColumnSchema struct {
-	Name         string
-	DataType     string
-	IsNullable   bool
-	IsPrimaryKey bool
+	Name         string `json:"name,omitempty"`
+	DataType     string `json:"dataType,omitempty"`
+	IsNullable   bool   `json:"isNullable,omitempty"`
+	IsPrimaryKey bool   `json:"isPrimaryKey,omitempty"`
 }
